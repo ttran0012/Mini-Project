622f9f6ad23d27174c2333d82efb99c043ad589d
Tan Tran <tantran@Tans-MacBook-Air.local>
2026-03-16 19:57:13 -0400

    CCGPA INIT

---
+++ AAA/AAA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AAA
+//
+//  Created by Tan Tran on 3/12/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
