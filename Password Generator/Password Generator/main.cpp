a3033363396920790869e540723f04e3a1f60228
Tan Tran <tantran@Tans-MacBook-Air.local>
2026-03-16 21:15:37 -0400

    Password Generator

--- Password Generator/Password Generator/main.cpp
+++ Password Generator/Password Generator/main.cpp
@@ -6,9 +6,55 @@
 //
 
 #include <iostream>
+#include <string>
+#include <random>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+    
+    int passLength;
+    std::cout << "Enter Password length: " << std::endl;
+    std::cin >> passLength;
+    
+    char choice;
+    
+    std::cout << "Include uppercase? (y/n): " << std::endl;
+    std::cin >> choice;
+    bool isIncludeUppercase = (choice == 'y' || choice == 'Y');
+    
+    std::cout << "Include lowercase? (y/n): " << std::endl;
+    std::cin >> choice;
+    bool isIncludeLowercase = (choice == 'y' || choice == 'Y');
+    
+    std::cout << "Include Numbers? (y/n): " << std::endl;
+    std::cin >> choice;
+    bool isIncludeNumbers = (choice == 'y' || choice == 'Y');
+    
+    std::cout << "Include symbols? (y/n): " << std::endl;
+    std::cin >> choice;
+    bool isIncludeSymbols = (choice == 'y' || choice == 'Y');
+    
+    std::string pool;
+
+    if(isIncludeUppercase) pool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if(isIncludeLowercase) pool += "abcdefghijklmnopqrstuvwxyz";
+    if(isIncludeNumbers) pool += "0123456789";
+    if(isIncludeSymbols) pool += "!@#$%^&*";
+    if(pool.empty()){
+        std::cout << "Error: Select at least one option!\n" << std::endl;
+        return 0;
+    }
+    
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> dist(0, pool.size() - 1);
+    std::string password = "";
+    for (int i =0; i < passLength; i++){
+        
+        password += pool[dist(rng)];
+    }
+    
+    std::cout << "Password: " << password << "\n" << std::endl;
+    
+    return 0;
 }
+
